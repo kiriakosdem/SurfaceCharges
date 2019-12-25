3e2eaf02dc67ddb722fad16634d9b1205b46f20a
kiriakosdem <52723766+kiriakosdem@users.noreply.github.com>
2019-12-25 20:06:56 +0200

    Add files via upload

--- BouncingBall.js
+++ BouncingBall.js
@@ -10,7 +10,11 @@ class BouncingBall {
 		this.speedx = vx;
 		this.speedy = vy;
 		this.speedz = vz;
-		this.charge = 1;
+		if ((vx*vx + vy*vy + vz*vz)==0){
+			this.charge = 1;
+		} else {
+			this.charge = -1;
+		}
 	}
 
 
@@ -22,20 +26,17 @@ class BouncingBall {
 
 
 	show() {
-		//2D
-		//fill(map(velocitymag,0,2*this.maxspeed,0,255), 0, map(velocitymag,0,2*this.maxspeed,255,0));
-		//let alpha = map(this.z,0,boxdepth,0,255); 
-		//fill(alpha,0,255-alpha);
-		//strokeWeight(1);
-		//stroke(0,0,0,0);
-		//ellipse(this.x, this.y, this.diameter, this.diameter);
-		
-		//3D 
+		let speed=Math.sqrt(this.speedx*this.speedx+this.speedy*this.speedy+this.speedz*this.speedz); 
 		translate(this.x,this.y,this.z);
 		//fill(0,0,50);
-		specularMaterial(0,0,100);
+		if (this.charge==-1){
+			specularMaterial(0,0,100);
+		}else{
+			specularMaterial(100,0,0);
+		}
+
 		noStroke();
-		let conductor = sphere(this.diameter);
+		sphere(this.radius);
 		translate(-this.x,-this.y,-this.z);
 	}	
 
@@ -44,54 +45,156 @@ class BouncingBall {
 		let d = Math.sqrt(this.x*this.x + this.y*this.y + this.z*this.z);
 		//radial velocity
 		let rv = this.x*this.speedx/d + this.y*this.speedy/d + this.z*this.speedz/d;
-		
+
 		if (d> sphereDiameter - this.radius){
 			//move back into the sphere
 			let exsp = d - sphereDiameter + this.radius;
 			this.x -= exsp*this.x/d;
 			this.y -= exsp*this.y/d;
 			this.z -= exsp*this.z/d;
-			
+
 			//reverse radial velocity
 			this.speedx -= 2*rv*this.x/d;
 			this.speedy -= 2*rv*this.y/d;
 			this.speedz -= 2*rv*this.z/d;
+
+			this.speedx *=  colissionElasticity;
+			this.speedy *=  colissionElasticity;
+			this.speedz *=  colissionElasticity;
 		}
 	}
+
+	cylinderColission(){
+		//distance from center in yz plane
+		let d = Math.sqrt(this.y*this.y + this.z*this.z);
+		//radial velocity in yz plane
+		let rv = this.y*this.speedy/d + this.z*this.speedz/d;
+		//side of cylinder
+		if (d> cylinderDiameter - this.radius){
+			//move back into the cylinder
+			let exsp = d - cylinderDiameter + this.radius;
+			this.y -= exsp*this.y/d;
+			this.z -= exsp*this.z/d;
+
+			//reverse radial velocity
+			this.speedy -= 2*rv*this.y/d;
+			this.speedz -= 2*rv*this.z/d;
+
+			this.speedx *=  colissionElasticity;
+			this.speedy *=  colissionElasticity;
+			this.speedz *=  colissionElasticity;
+		}
+		//bottom cap
+		if (this.x > x0 + cylinderHeight/2 - this.radius) {
+			this.x = x0 + cylinderHeight/2 - this.radius;
+			this.speedx *= -1;
+
+			this.speedx *=  colissionElasticity;
+			this.speedy *=  colissionElasticity;
+			this.speedz *=  colissionElasticity;
+		}
+		//top cap
+		if (this.x < x0 - cylinderHeight/2 + this.radius) {
+			this.x = x0 - cylinderHeight/2 + this.radius;
+			this.speedx *= -1;
+
+			this.speedx *=  colissionElasticity;
+			this.speedy *=  colissionElasticity;
+			this.speedz *=  colissionElasticity;
+		}
+
+
+	}
+
 
 	boxcolission() {
 		//bottom wall
-		if (this.y > ybox + boxheight/2 - this.radius) {
-			this.y = ybox + boxheight/2 - this.radius;
+		if (this.y > y0 + boxheight/2 - this.radius) {
+			this.y = y0 + boxheight/2 - this.radius;
 			this.speedy *= -1;
+			this.speedx *=  colissionElasticity;
+			this.speedy *=  colissionElasticity;
+			this.speedz *=  colissionElasticity;
 		}
 		//top wall
-		if (this.y < ybox - boxheight/2 + this.radius) {
-			this.y = ybox - boxheight/2 + this.radius;
+		if (this.y < y0 - boxheight/2 + this.radius) {
+			this.y = y0 - boxheight/2 + this.radius;
 			this.speedy *= -1;
+			this.speedx *=  colissionElasticity;
+			this.speedy *=  colissionElasticity;
+			this.speedz *=  colissionElasticity;
 		}
 		//right wall
-		if (this.x > xbox + boxwidth/2 - this.radius) {
-			this.x = xbox + boxwidth/2 - this.radius;
+		if (this.x > x0 + boxwidth/2 - this.radius) {
+			this.x = x0 + boxwidth/2 - this.radius;
 			this.speedx *= -1;
+			this.speedx *=  colissionElasticity;
+			this.speedy *=  colissionElasticity;
+			this.speedz *=  colissionElasticity;
 		}
 		//left wall
-		if (this.x < xbox - boxwidth/2 + this.radius) {
-			this.x = xbox - boxwidth/2 + this.radius;
+		if (this.x < x0 - boxwidth/2 + this.radius) {
+			this.x = x0 - boxwidth/2 + this.radius;
 			this.speedx *= -1;
+			this.speedx *=  colissionElasticity;
+			this.speedy *=  colissionElasticity;
+			this.speedz *=  colissionElasticity;
 		}
 		//front wall
-		if (this.z > zbox + boxdepth/2 - this.radius) {
-			this.z = zbox + boxdepth/2 - this.radius;
+		if (this.z > z0 + boxdepth/2 - this.radius) {
+			this.z = z0 + boxdepth/2 - this.radius;
 			this.speedz *= -1;
+			this.speedx *=  colissionElasticity;
+			this.speedy *=  colissionElasticity;
+			this.speedz *=  colissionElasticity;
 		}
 		//back wall
-		if (this.z < zbox - boxdepth/2 + this.radius) {
-			this.z = zbox - boxdepth/2 + this.radius;
+		if (this.z < z0 - boxdepth/2 + this.radius) {
+			this.z = z0 - boxdepth/2 + this.radius;
 			this.speedz *= -1;
+			this.speedx *=  colissionElasticity;
+			this.speedy *=  colissionElasticity;
+			this.speedz *=  colissionElasticity;
 		}
+
+
 	}
 
+
+	staticcolission(otherball) {
+		// calculate distance vector
+		let dx = this.x - otherball.x;
+		let dy = this.y - otherball.y;
+		let dz = this.z - otherball.z;
+		let d = Math.sqrt(dx*dx + dy*dy + dz*dz);
+
+		if (d <= (this.radius + otherball.radius)){
+			// speed on the line of colission 
+			let vx1 = (dx*this.speedx+dy*this.speedy+dz*this.speedz)/pow(d,2)*dx;
+			let vy1 = (dx*this.speedx+dy*this.speedy+dz*this.speedz)/pow(d,2)*dy;
+			let vz1 = (dx*this.speedx+dy*this.speedy+dz*this.speedz)/pow(d,2)*dz;
+
+
+			// space them out, so they don't overlap
+			let exsp = (this.radius+otherball.radius)-d;
+			//print(d);
+			this.x += exsp/2*dx/d;
+			this.y += exsp/2*dy/d; 
+			this.z += exsp/2*dz/d;
+
+			// invert speed
+			this.speedx +=  -2*vx1;
+			this.speedy +=  -2*vy1;
+			this.speedz +=  -2*vz1;
+
+			this.speedx *= colissionElasticity/10;
+			this.speedy *= colissionElasticity/10;
+			this.speedz *= colissionElasticity/10;
+
+		}
+
+
+	}
 
 	ballcolission(otherball) {
 		// calculate distance vector
@@ -104,19 +207,19 @@ class BouncingBall {
 			// speed on the line of colission 
 			let vx1 = (dx*this.speedx+dy*this.speedy+dz*this.speedz)/pow(d,2)*dx;
 			let vy1 = (dx*this.speedx+dy*this.speedy+dz*this.speedz)/pow(d,2)*dy;
-			let vz1 = (dx*this.speedx+dy*this.speedy+dz*this.speedz)/pow(d,2)*dz
+			let vz1 = (dx*this.speedx+dy*this.speedy+dz*this.speedz)/pow(d,2)*dz;
 			let vx2 = (dx*otherball.speedx+dy*otherball.speedy+dz*otherball.speedz)/pow(d,2)*dx;
 			let vy2 = (dx*otherball.speedx+dy*otherball.speedy+dz*otherball.speedz)/pow(d,2)*dy;
 			let vz2 = (dx*otherball.speedx+dy*otherball.speedy+dz*otherball.speedz)/pow(d,2)*dz;
 
 			// space them out, so they don't overlap
 			let exsp = (this.radius+otherball.radius)-d;
-			this.x += exsp/2*dx/d;
-			this.y += exsp/2*dy/d; 
-			this.z += exsp/2*dz/d;
-			otherball.x -= exsp/2*dx/d;
-			otherball.y -= exsp/2*dy/d;
-			otherball.z -= exsp/2*dz/d;
+			this.x += exsp*dx/d;
+			this.y += exsp*dy/d; 
+			this.z += exsp*dz/d;
+			otherball.x -= exsp*dx/d;
+			otherball.y -= exsp*dy/d;
+			otherball.z -= exsp*dz/d;
 
 			// swap speeds
 			this.speedx += - vx1 + vx2;
@@ -125,29 +228,32 @@ class BouncingBall {
 			otherball.speedx += - vx2 + vx1;
 			otherball.speedy += - vy2 + vy1;
 			otherball.speedz += - vz2 + vz1;
+
+			this.speedx *=  colissionElasticity;
+			this.speedy *=  colissionElasticity;
+			this.speedz *=  colissionElasticity;
 		}
 		// return (d < (this.radius + otherball.radius));
 	}
 
 
-	gravity() {
-		this.speedy += 0.10;
+	potential() {
+		this.speedx += voltageStrength;
 	}
 
 
 	friction() {
-		let totalspeed = Math.sqrt(pow(this.speedx,2) + pow(this.speedy,2) + pow(this.speedy,2));
-		let smallspeed = 0.99;
-		let bigspeed = 0.95;
+		this.speedx *= frictionCoefficient;
+		this.speedy *= frictionCoefficient;
+		this.speedz *= frictionCoefficient;
+	}
 
-		if (totalspeed < 0.1) {
-			this.speedx *= smallspeed;
-			this.speedy *= smallspeed;
-			this.speedz *= smallspeed;
-		} else {
-			this.speedx *= bigspeed;
-			this.speedy *= bigspeed;
-			this.speedz *= bigspeed;
+	speedCheck(){
+		let speed=Math.sqrt(this.speedx*this.speedx+this.speedy*this.speedy+this.speedz*this.speedz);
+		if (speed>speedlimit){
+			this.speedx *= speedlimit/speed;
+			this.speedy *= speedlimit/speed;
+			this.speedz *= speedlimit/speed;
 		}
 	}
 
@@ -160,10 +266,10 @@ class BouncingBall {
 		let d = Math.sqrt(dx*dx + dy*dy + dz*dz);
 
 		//calculate force
-		if (d>3*this.radius){
-			this.speedx += this.charge*forceStrength*dx/pow(d,3);
-			this.speedy += this.charge*forceStrength*dy/pow(d,3);
-			this.speedz += this.charge*forceStrength*dz/pow(d,3);
+		if (d>=(this.radius+otherball.radius)){
+			this.speedx += forceStrength*this.charge*otherball.charge*dx/pow(d,3);
+			this.speedy += forceStrength*this.charge*otherball.charge*dy/pow(d,3);
+			this.speedz += forceStrength*this.charge*otherball.charge*dz/pow(d,3);
 		}
 	}
 
--- sketch.js
+++ sketch.js
@@ -3,26 +3,41 @@ let fullscreenwidth = document.getElementById("jscode").clientWidth;//myscreen:1
 let fullscreenheight = document.getElementById('jscode').clientHeight;//myscreen:657;
 let animationwidth = fullscreenwidth;
 let animationheight = fullscreenheight*0.72;
-
-//
-let boxwidth = 500;
-let boxheight = 500;
-let boxdepth = 500;
-let xbox = 0;
-let ybox = 0;
-let zbox = 0;
-let sphereDiameter = 300;
-let conductor;
-
-//animation variables
-let pi = 3.1415;
-let balls = [];
-let Nparticles;
-let forceStrength;
-let forceCoefficient = 100*100*10;
-let angle = 0;
 let sensitivityZoom = 0.06;
-let img;
+let perspectiveScale = 0.8;
+
+//animation parameters (change at your own risk)
+let pi = 3.1415;
+let forceStrength;
+let forceCoefficient = 100000;
+let frictionCoefficient = 1;
+let voltageStrength = 0.8;
+let voltageActive = false;
+let colissionElasticity = 0.9;
+
+//electrons
+let particles = [];
+let atoms = [];
+let particlesNumber;
+let atomsNumber;
+let particleRadius = animationheight/100;
+let particleDiameter = particleRadius*2;
+let atomRadius = particleRadius*1;
+let atomDiameter = atomRadius*2;
+let velocity = 20;
+let speedlimit = 20;
+	
+//conductor 
+let boxwidth = 700;
+let boxheight = 400;
+let boxdepth = 300;
+let sphereDiameter = 300;
+let cylinderHeight = 800;
+let cylinderDiameter = 150;
+let x0 = 0;
+let y0 = 0;
+let z0 = 0;
+let shape = 1;
 
 //html elements
 let mycanvas;
@@ -31,10 +46,6 @@ let sliderRotateX;
 let sliderRotateY;
 let sliderRotateZ;
 
-
-function preload(){
-	img = loadImage('images/lightning.png');
-}
 
 
 function setup() {
@@ -45,19 +56,33 @@ function setup() {
 	header.style('padding-bottom','0px');
 	header.parent('jscode');
 
-	par = createP('Τα φορτία σε έναν αγωγό καταλήγουν όλα στην επιφάνειά του!');
+	par = createP('Τα επιπλέον φορτία σε έναν αγωγό καταλήγουν όλα στην επιφάνειά του κι αν βρεθούν σε κάποιο ηλεκτρικό πεδίο ανακατανέμονται ώστε να το εξουδετερώσουν!');
 	par.style('padding-left','10px');
 	par.parent('jscode');
-	
+
 	setAttributes('antialias', true);
 	mycanvas = createCanvas(animationwidth, animationheight, WEBGL);
 	mycanvas.parent("jscode");
 
-	sliderParticles = createSlider(1, 300);
+	sliderParticles = createSlider(1, 300, 50);
 	sliderParticles.parent('jscode');
-	sliderParticles.style('width', '200px');
+	sliderParticles.style('width', '250px');
 	sliderParticles.style('margin','20px');
 	sliderParticles.class('slider sliderNumber');
+	
+	labelParticles = createElement('label','Αριθμός Φορτίων: ');
+	labelParticles.parent('jscode');
+	labelParticles.position(sliderParticles.position().x,sliderParticles.position().y+1.5*sliderParticles.height);
+//	
+//	sliderAtoms = createSlider(0, 300,0);
+//	sliderAtoms.parent('jscode');
+//	sliderAtoms.style('width', '250px');
+//	sliderAtoms.style('margin','20px');
+//	sliderAtoms.class('slider sliderNumber');
+//	
+//	labelAtoms = createElement('label','Αριθμός Ατόμων: ');
+//	labelAtoms.parent('jscode');
+//	labelAtoms.position(sliderAtoms.position().x,sliderAtoms.position().y+1.5*sliderAtoms.height);
 
 	buttonReset = createButton("Επαναφορά Φορτίων");
 	buttonReset.parent("jscode");
@@ -72,10 +97,24 @@ function setup() {
 	buttonResetCam.style('margin','20px');
 	buttonResetCam.class('mybutton');
 	buttonResetCam.mousePressed(resetCamera);
+
+	buttonChangeShape = createButton("Άλλαξε Σχήμα");
+	buttonChangeShape.parent("jscode");
+	buttonChangeShape.style('width', '120px');
+	buttonChangeShape.style('margin','20px');
+	buttonChangeShape.class('mybutton');
+	buttonChangeShape.mousePressed(nextShape);
 	
-	
-	camera(0,0,2.7*(height/2.0)/tan(PI*30.0/180.0),  0,0,0,  0,1,0);
-	perspective(0.6);
+	buttonPotential = createButton("Εφαρμογή Τάσης");
+	buttonPotential.parent("jscode");
+	buttonPotential.style('width', '130px');
+	buttonPotential.style('margin','20px');
+	buttonPotential.class('mybutton');
+	buttonPotential.mousePressed(toggleVoltage);
+
+
+	camera(0,0,2*(height/2.0)/tan(PI*30.0/180.0),  0,0,0,  0,1,0);
+	perspective(perspectiveScale);
 	resetSketch();
 }
 
@@ -84,105 +123,155 @@ function draw() {
 	//set the scene
 	background(0, 60, 70);
 	ambientLight(255, 255, 255);
-	//pointLight(255,255,255, 0,0,0);	
+	pointLight(255,255,255, 0,0,0);	
 	orbitControl();
 	//debugMode();
-	rotateX(angle);
-	rotateY(angle);
-	rotateZ(angle);
-
-	//create particles
-	if (sliderParticles.value()!=balls.length){
+	//sphere(10,10);
+	
+	//create particles if needed
+	if (sliderParticles.value()!=particles.length){
 		resetSketch();
 	}
-
-	for (var ind = 0; ind < balls.length; ind++) {
-		// draw shapes
-		balls[ind].show();
-
-		// motion
-		balls[ind].move();
-
-		// check for colissions
-		//balls[ind].boxcolission();
-		balls[ind].spherecolission();
-		for (let indother = 0; indother < ind; indother++) {
-			balls[ind].ballcolission(balls[indother]);
+//	if (sliderAtoms.value()!=atoms.length){
+//		resetSketch();
+//	}
+	
+	//show and move 
+	for (var ind = 0; ind < particles.length; ind++) {
+		particles[ind].show();
+		particles[ind].move();
+	}
+	for (var ind = 0; ind < atoms.length; ind++){
+		atoms[ind].show();
+		atoms[ind].move();
+	}
+	
+	//calculate forces
+	for (var ind = 0; ind < particles.length; ind++) {
+		
+		//colissions with conductor walls
+		if (shape == 1){
+			particles[ind].boxcolission();
+		} else if (shape == 2){
+			particles[ind].spherecolission();
+		} else if (shape >= 3){
+			particles[ind].cylinderColission();
 		}
-
-		// other forces
-		//balls[ind].gravity();
-		balls[ind].friction();
-		for (let indother = 0; indother < balls.length; indother++) {
+		
+		
+		//first, repulsion between balls
+		for (let indother = 0; indother <particles.length; indother++) {
 			if (indother!=ind){
-				balls[ind].repulsion(balls[indother]);
+				particles[ind].repulsion(particles[indother]);
 			}	
 		}
+		//next, attraction between balls and atoms
+		for (let indother = 0; indother < atoms.length; indother++) {	
+			particles[ind].repulsion(atoms[indother]);
+		}
+		
+		
+		//next, colission between balls
+		for (let indother = 0; indother < ind; indother++) {
+			particles[ind].ballcolission(particles[indother]);
+		}
+		//colission between balls and atoms
+		for (let indother = 0; indother < atoms.length; indother++) {
+			particles[ind].staticcolission(atoms[indother]);
+		}
+		
+		
+		//other forces
+		particles[ind].friction();
+		if (voltageActive){
+			particles[ind].potential();
+		}	
+		
+		//last, speed check
+		particles[ind].speedCheck();
 	}
 
-	//draw box
+	//draw conductor
 	fill(72,45,20,100);
 	stroke(18,11,5);
 	strokeWeight(1);
-	rectMode(CENTER);
-	translate(xbox,ybox,zbox);
-	//box(boxwidth,boxheight,boxdepth);
-	translate(-xbox,-ybox,-zbox);
-	//noStroke();
-	conductor  = sphere(sphereDiameter);
-	//torus(300,30,4,12);
+	
+	translate(x0,y0,z0);
+	if (shape == 1){	
+		box(boxwidth,boxheight,boxdepth);
+	} else if (shape == 2){
+		sphere(sphereDiameter);
+	} else if (shape == 3){
+		push();
+		rotateZ(PI/2);
+		cylinder(cylinderDiameter,cylinderHeight);
+		pop();
+	}
+	translate(-x0,-y0,-z0);
 
-	//angle += 0.0005;
 }
 
+function nextShape() {
+	shape += 1;
+	if (shape > 3){
+		shape = 1;
+	}
+	if (shape==1){
+		buttonChangeShape.html('Κουτί');
+	}else if (shape == 2){
+		buttonChangeShape.html('Σφαίρα');
+	}else if (shape ==3){
+		buttonChangeShape.html('Κύλινδρος');
+	}
+}
+
+function toggleVoltage(){
+	voltageActive = !voltageActive;
+	if (voltageActive){
+		buttonPotential.html("Τάση: On");
+	} else {
+		buttonPotential.html("Τάση: Off");
+	}
+}
+
+
 function resetCamera(){
-	camera(0,0,2.7*(height/2.0)/tan(PI*30.0/180.0),  0,0,0,  0,1,0);
-	angle = 0;
+	camera(0,0,2*(height/2.0)/tan(PI*30.0/180.0),  0,0,0,  0,1,0);
 }
 
 
 function resetSketch() {
 	//delete previous particles
-	balls = [];
-	Nparticles = sliderParticles.value();
-	forceStrength = forceCoefficient/Nparticles;
+	particles = [];
+	atoms = [];
+	particlesNumber = sliderParticles.value();
+	//atomsNumber = sliderAtoms.value();;
+	atomsNumber = 0 ;
+	labelParticles.html("Αριθμός Φορτίων: " + particlesNumber);
+	//labelAtoms.html("Αριθμός Ατόμων: " + atomsNumber);
+	
+	forceStrength = forceCoefficient/particlesNumber;
 
 	//create new particles
-	let ip;
-	let ip2;
-	for (ip=0; ip<Nparticles; ip++){
-
-		let diameter = boxwidth/50;
-		let radius = diameter/2;
-		let x = xbox + random(-boxwidth/2 + radius, boxwidth/2-radius);
-		let y = ybox + random(-boxheight/2 + radius, boxheight/2-radius);
-		let z = zbox + random(-boxdepth/2 + radius, boxdepth/2-radius);
-
-//		if (ip>0){
-//			valid = false;
-//			while (valid==false){
-//				x = random(radius,boxwidth-radius);
-//				y = random(radius,boxwidth-radius);
-//				z = random(radius,boxwidth-radius);
-//				overlap = false;
-//				for (ip2=0; ip2<balls.length; ip2++){
-//					dist = Math.sqrt(pow((x-balls[ip2].x),2)+pow((y-balls[ip2].y),2)+pow((z-balls[ip2].z),2))
-//					if (dist<=diameter){
-//						overlap = true;
-//					}
-//				}
-//				if (overlap == false){
-//					valid = true;
-//				}
-//			}
-//		}
-
-		let velocity = 10;
+	for (let ip=0; ip<particlesNumber; ip++){	
+		let x = x0 + random(-boxwidth/2 + particleRadius, boxwidth/2-particleRadius);
+		let y = y0 + random(-boxheight/2 + particleRadius, boxheight/2-particleRadius);
+		let z = z0 + random(-boxdepth/2 + particleRadius, boxdepth/2-particleRadius);	
 		let vx = random(-velocity, velocity)/Math.sqrt(3);
 		let vy = random(-velocity, velocity)/Math.sqrt(3);
 		let vz = random(-velocity, velocity)/Math.sqrt(3);
-
-		balls.push(new BouncingBall(x,y,z,vx,vy,vz,diameter));
+		particles.push(new BouncingBall(x,y,z,vx,vy,vz,particleDiameter));
+	}
+	
+	//create atoms
+	for (let ip=0; ip<atomsNumber; ip++){	
+		let x = x0 + random(-cylinderHeight/2 + atomRadius, cylinderHeight/2-atomRadius);
+		let y = y0 + random(-cylinderDiameter + atomRadius, cylinderDiameter-atomRadius);
+		let z = z0 + random(-cylinderDiameter + atomRadius, cylinderDiameter-atomRadius);	
+		let vx = 0;
+		let vy = 0;
+		let vz = 0;
+		atoms.push(new BouncingBall(x,y,z,vx,vy,vz,atomDiameter));
 	}
 }
 
